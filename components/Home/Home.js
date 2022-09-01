2a1de47c43d32b71727b8a7b62a5bfcb2aa07b36
Dhannyphantom <youngskillzzz@gmail.com>
2022-09-01 14:48:05 +0100

    Attempting transition to NEXT.JS project

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "next/link";
 import styles from "./Home.module.css";
 
 const Home = () => {
--- components/Toppings/Toppings.js
+++ components/Toppings/Toppings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "next/link";
 import styles from "./Toppings.module.css";
 
 const Toppings = ({ addTopping, pizza }) => {
