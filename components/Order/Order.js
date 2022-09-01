ef99e95397d890a6b0ea0da725f83ef2c946623b
Dhannyphantom <youngskillzzz@gmail.com>
2022-09-01 20:18:39 +0100

    Toppings Component starter setup done (All layout starter structure is done)

--- components/Order/Order.js
+++ components/Order/Order.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Order.module.css";
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza = { base: "Classic", toppings: [] } }) => {
   return (
     <div className={styles.container}>
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
       {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
+        <div className={styles.topping} key={topping}>
+          {topping}
+        </div>
       ))}
     </div>
   );
